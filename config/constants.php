409399585c1c8f0ed4ed8bcc8940d48ed9b389a2
willise414 <willise414@gmail.com>
2023-07-11 17:07:26 -0300

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -4,6 +4,6 @@ session_start();
 define('ROOT_URL', 'http://localhost/blog-php/');
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'willise');
-define('DB_PASS', 'Mjtgfde3');
+define('DB_USER', '');
+define('DB_PASS', '');
 define('DB_NAME', 'blog');
